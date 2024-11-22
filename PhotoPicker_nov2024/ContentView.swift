609c47e9d58c3da887e7cfc0a6012bdbd9ec3383
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-11-22 12:30:05 +0530

    Initial Commit

---
+++ PhotoPicker_nov2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoPicker_nov2024
+//
+//  Created by mac on 22/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoPicker_nov2024/PhotoPicker_nov2024App.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPicker_nov2024App.swift
+//  PhotoPicker_nov2024
+//
+//  Created by mac on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPicker_nov2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
